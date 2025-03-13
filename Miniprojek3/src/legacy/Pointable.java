6a5bb4719f0ffa4e63b6ef7ab2b277900b42a079
GTSA - Infinity <williamlindahl03@gmail.com>
2025-03-13 13:44:53 +0100

    commit

---
+++ Miniprojek3/src/legacy/Pointable.java
@@ -0,0 +1,9 @@
+package legacy;
+
+
+public interface Pointable
+  {
+  public void pointerDown(Point point);
+  public void pointerUp(Point point);
+  public void pointerMoved(Point point, boolean pointerDown);
+  }
